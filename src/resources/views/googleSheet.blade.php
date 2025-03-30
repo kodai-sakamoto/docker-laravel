4ed38f37302df1ff4e45c3f153652b1982422453
坂本航大 <sakamotoworks22@gmail.com>
2025-03-30 17:19:45 +0900

    #1 gcp

---
+++ src/resources/views/googleSheet.blade.php
@@ -0,0 +1,13 @@
+<!-- <!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Google Sheet 表示ページ</title>
+</head>
+<body>
+    <h1>Google Sheet のデータを表示するページ</h1>
+    <p>ここに Google Sheet のデータを表示できます。</p>
+
+    <a href="{{ route('home') }}">ホームへ戻る</a>
+</body>
+</html> -->
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/google-sheet', [GoogleSheetController::class, 'index'])->name('google.sheet');
